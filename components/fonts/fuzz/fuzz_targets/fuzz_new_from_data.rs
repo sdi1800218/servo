f71d669855f081a56b9eb42989f896d92324bcf8
Harry Hello <hello@hello.cc>
2024-07-18 14:19:09 +0300

    Fixed new_from_data() fuzz test

--- components/fonts/fuzz/fuzz_targets/fuzz_new_from_data.rs
+++ components/fonts/fuzz/fuzz_targets/fuzz_new_from_data.rs
@@ -2,8 +2,13 @@
 use libfuzzer_sys::fuzz_target;
 use fonts::*;
 use std::sync::Arc;
+use fonts::platform::font::PlatformFont;
+//use fonts::FontIdentifier;
+use servo_url::ServoUrl;
+
 
 // Fuzz target definition
+/*
 fuzz_target!(|data: (FontIdentifier, Vec<u8>, u32, Option<Au>)| {
     let font_identifier = data.0;
     let font_data = Arc::new(data.1);
@@ -11,6 +16,14 @@ fuzz_target!(|data: (FontIdentifier, Vec<u8>, u32, Option<Au>)| {
     let pt_size = data.3;
 
     // Send them in
-    let _ = YourPlatformFont::new_from_data(font_identifier, font_data, face_index, pt_size);
+    let _ = PlatformFont::new_from_data(font_identifier, font_data, face_index, pt_size);
 });
+*/
 
+fuzz_target!(|data: &[u8]| {
+    let path = format!("/tmp/fuzz_font_{}.ttf", 0); // Static path for simplicity
+    let identifier = FontIdentifier::Web(ServoUrl::from_file_path(path.clone()).unwrap());
+
+    // Attempt to create a PlatformFont with the provided data
+    let _ = PlatformFont::new_from_data(identifier, Arc::new(data.to_vec()), 0, None);
+});
